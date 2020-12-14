68706a3750fb29b0607a7893ae94fc4ac24f78f4
Bryant Friend <bryantfriend@yahoo.com>
2020-12-13 16:58:28 -0800

    added email js

---
+++ btnSubmit.js
@@ -0,0 +1,7 @@
+function submit(){
+    let fName = document.getElementById('fName').nodeValue
+    let lName = document.getElementById('lName').nodeValue
+    let clinic = document.getElementById('clinic').nodeValue
+    let subject = document.getElementById('subject').nodeValue
+    window.open('mailto:info@lgn.com?subject=subject&body=body');
+}
