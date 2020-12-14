6cd1e86bb7467b2e9d05a673aa6cfa15bf80ab30
Bryant Friend <bryantfriend@yahoo.com>
2020-12-13 17:45:07 -0800

    Update btnSubmit.js

--- btnSubmit.js
+++ btnSubmit.js
@@ -1,10 +1,10 @@
 alert('test 1');
 
-function submit(){
-    alert ("test 2");
-    let fName = document.getElementById('fName').nodeValue
-    let lName = document.getElementById('lName').nodeValue
-    let clinic = document.getElementById('clinic').nodeValue
-    let subject = document.getElementById('subject').nodeValue
-    window.open('mailto:info@lgn.com?subject=subject&body=body');
-};
+//function submit(){
+//    alert ("test 2");
+//    let fName = document.getElementById('fName').nodeValue
+//    let lName = document.getElementById('lName').nodeValue
+//    let clinic = document.getElementById('clinic').nodeValue
+//    let subject = document.getElementById('subject').nodeValue
+//    window.open('mailto:info@lgn.com?subject=subject&body=body');
+//};
